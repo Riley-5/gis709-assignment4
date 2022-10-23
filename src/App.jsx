34f78e36896e6e44608d9894ecb48629749b0ec0
Riley <riley.kamstra@gmail.com>
2022-10-23 17:53:19 +0200

    Added geoJSON layer to map from Geoserver

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import "leaflet/dist/leaflet.css"
-import React from "react"
-import { MapContainer, TileLayer } from "react-leaflet"
+import React, { useEffect, useState } from "react"
+import { GeoJSON, MapContainer, TileLayer } from "react-leaflet"
 import "./style.css"
 
 const App = () => {
+	// State to hold the roads
+	const [routes, setRoutes] = useState(null)
+
+	/*
+        Collect data from Geoserver
+        Set the routes state to the received data
+    */
+	useEffect(() => {
+		fetch(
+			"http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Aways&outputFormat=application%2Fjson"
+		)
+			.then((response) => response.json())
+			.then((data) => setRoutes(data))
+	}, [])
+
 	return (
 		<MapContainer center={[-25.7487, 28.238]} zoom={15}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
+			{/* 
+                If there are routes stored in state return GeoJSON layer 
+                Else return null
+            */}
+			{routes ? <GeoJSON data={routes} /> : null}
 		</MapContainer>
 	)
 }
