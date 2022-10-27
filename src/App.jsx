a8e65c59314caab11d2b3ac63de0a4a2a65d604e
Riley <riley.kamstra@gmail.com>
2022-10-27 21:46:48 +0200

    Added end marker

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,10 @@ L.Marker.prototype.options.icon = DefaultIcon
 const App = () => {
 	// State to hold the roads
 	const [routes, setRoutes] = useState(null)
-	const [nearestVertex, setNearestVertex] = useState(null)
+	const [startNearestVertex, setStartNearestVertex] = useState(null)
+	const [endNearestVertex, setEndNearestVertex] = useState(null)
+	const [startMarkerCoords, setStartMarkerCoords] = useState([-25.7487, 28.238])
+	const [endMarkerCoords, setEndMarkerCoords] = useState([-25.75, 28.25])
 
 	/*
         Collect data from Geoserver
@@ -26,36 +29,67 @@ const App = () => {
     */
 	useEffect(() => {
 		/*
+			startNearestVertex viewparams:
+			x -> latitude
+			y -> longitude
+		*/
+		fetch(
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:-25.7487;y:28.238`
+		)
+			.then((response) => response.json())
+			.then((data) =>
+				data.features.forEach((point) => {
+					setStartNearestVertex(point.properties.id)
+				})
+			)
+
+		/*
+			endNearestVertex viewparams:
+			x -> latitude
+			y -> longitude
+		*/
+		fetch(
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:-25.75;y:28.25`
+		)
+			.then((response) => response.json())
+			.then((data) =>
+				data.features.forEach((point) => {
+					setEndNearestVertex(point.properties.id)
+				})
+			)
+
+		/*
 			viewparams:
 			source -> starting node id
 			target -> end node id
 		*/
-		// fetch(
-		// 	"http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Ashortest_route&outputFormat=application%2Fjson&viewparams=source:498;target:368"
-		// )
-		// 	.then((response) => response.json())
-		// 	.then((data) => setRoutes(data))
-		/*
-			viewparams:
-			x -> latitude
-			y -> longitude
-		*/
-		// fetch(
-		// 	"http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:-25.7487;y:28.238"
-		// )
-		// 	.then((response) => response.json())
-		// 	.then((data) => setNearestVertex(data))
-	}, [])
+		fetch(
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Ashortest_route&outputFormat=application%2Fjson&viewparams=source:${5};target:${164}`
+		)
+			.then((response) => response.json())
+			.then((data) => setRoutes(data))
+	}, [startMarkerCoords, startNearestVertex, endMarkerCoords, endNearestVertex])
 
-	const [coords, setCoords] = useState([-25.7487, 28.238])
-	console.log(coords)
-	const markerRef = useRef(null)
-	const eventHandlers = useMemo(
+	const startMarkerRef = useRef(null)
+	const startMarkerEventHandlers = useMemo(
 		() => ({
 			dragend() {
-				const marker = markerRef.current
+				const marker = startMarkerRef.current
 				if (marker !== null) {
-					setCoords(marker.getLatLng())
+					setStartMarkerCoords(marker.getLatLng())
+				}
+			}
+		}),
+		[]
+	)
+
+	const endMarkerRef = useRef(null)
+	const endMarkerEventHandlers = useMemo(
+		() => ({
+			dragend() {
+				const marker = endMarkerRef.current
+				if (marker !== null) {
+					setEndMarkerCoords(marker.getLatLng())
 				}
 			}
 		}),
@@ -73,12 +107,17 @@ const App = () => {
                 Else return null
             */}
 			{routes ? <GeoJSON data={routes} /> : null}
-			{nearestVertex ? <GeoJSON data={nearestVertex} /> : null}
 			<Marker
 				draggable={true}
-				eventHandlers={eventHandlers}
-				position={coords}
-				ref={markerRef}
+				eventHandlers={startMarkerEventHandlers}
+				position={startMarkerCoords}
+				ref={startMarkerRef}
+			></Marker>
+			<Marker
+				draggable={true}
+				eventHandlers={endMarkerEventHandlers}
+				position={endMarkerCoords}
+				ref={endMarkerRef}
 			></Marker>
 		</MapContainer>
 	)
