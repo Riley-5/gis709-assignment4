2cce31ab91038ad41bb2ea0cc833e11ffeb46333
Riley <riley.kamstra@gmail.com>
2022-10-28 10:20:38 +0200

    Route renders when markers are changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import "leaflet/dist/leaflet.css"
 import React, { useEffect, useMemo, useRef, useState } from "react"
 import { GeoJSON, MapContainer, Marker, TileLayer } from "react-leaflet"
 import "./style.css"
 
-// Styling for leaflet marker
-import L from "leaflet"
-import icon from "leaflet/dist/images/marker-icon.png"
-import iconShadow from "leaflet/dist/images/marker-shadow.png"
-
-const DefaultIcon = L.icon({
-	iconUrl: icon,
-	shadowUrl: iconShadow
-})
-
-L.Marker.prototype.options.icon = DefaultIcon
-
 const App = () => {
 	// State to hold the roads
 	const [routes, setRoutes] = useState(null)
-	const [startNearestVertex, setStartNearestVertex] = useState(null)
-	const [endNearestVertex, setEndNearestVertex] = useState(null)
-	const [startMarkerCoords, setStartMarkerCoords] = useState([-25.7487, 28.238])
-	const [endMarkerCoords, setEndMarkerCoords] = useState([-25.75, 28.25])
+	const [startNearestVertex, setStartNearestVertex] = useState(190)
+	const [endNearestVertex, setEndNearestVertex] = useState(268)
+	const [startMarkerCoords, setStartMarkerCoords] = useState({
+		lat: -25.7527555,
+		lng: 28.2468943
+	})
+	const [endMarkerCoords, setEndMarkerCoords] = useState({
+		lat: -25.7521842,
+		lng: 28.2328684
+	})
 
 	/*
         Collect data from Geoserver
@@ -30,33 +23,33 @@ const App = () => {
 	useEffect(() => {
 		/*
 			startNearestVertex viewparams:
-			x -> latitude
-			y -> longitude
+			x -> longitude
+			y -> latitude
 		*/
 		fetch(
-			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:-25.7487;y:28.238`
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:${startMarkerCoords.lng};y:${startMarkerCoords.lat}`
 		)
 			.then((response) => response.json())
-			.then((data) =>
+			.then((data) => {
 				data.features.forEach((point) => {
 					setStartNearestVertex(point.properties.id)
 				})
-			)
+			})
 
 		/*
 			endNearestVertex viewparams:
-			x -> latitude
-			y -> longitude
+			x -> longitude
+			y -> latitude
 		*/
 		fetch(
-			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:-25.75;y:28.25`
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Anearest_vertex&outputFormat=application%2Fjson&viewparams=x:${endMarkerCoords.lng};y:${endMarkerCoords.lat}`
 		)
 			.then((response) => response.json())
-			.then((data) =>
+			.then((data) => {
 				data.features.forEach((point) => {
 					setEndNearestVertex(point.properties.id)
 				})
-			)
+			})
 
 		/*
 			viewparams:
@@ -64,7 +57,7 @@ const App = () => {
 			target -> end node id
 		*/
 		fetch(
-			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Ashortest_route&outputFormat=application%2Fjson&viewparams=source:${5};target:${164}`
+			`http://localhost:9090/geoserver/routes/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=routes%3Ashortest_route&outputFormat=application%2Fjson&viewparams=source:${startNearestVertex};target:${endNearestVertex}`
 		)
 			.then((response) => response.json())
 			.then((data) => setRoutes(data))
@@ -96,17 +89,17 @@ const App = () => {
 		[]
 	)
 
+	const Route = (props) => {
+		return <GeoJSON data={props.routes} />
+	}
+
 	return (
 		<MapContainer center={[-25.7487, 28.238]} zoom={15}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			{/* 
-                If there are routes stored in state return GeoJSON layer 
-                Else return null
-            */}
-			{routes ? <GeoJSON data={routes} /> : null}
+			<Route routes={routes} />
 			<Marker
 				draggable={true}
 				eventHandlers={startMarkerEventHandlers}
