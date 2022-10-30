704625b6892e2c3169c435dfe43a6ce2d4a0b869
Riley <riley.kamstra@gmail.com>
2022-10-30 15:51:17 +0200

    Added error checking for for the start end form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import L from "leaflet"
 import React, { useEffect, useMemo, useRef, useState } from "react"
 import { GeoJSON, MapContainer, Marker, TileLayer } from "react-leaflet"
 import Legend from "./components/Legend"
@@ -5,6 +6,29 @@ import SearchLocation from "./components/SearchLocation"
 import "./style.css"
 
 const App = () => {
+	// Green Leaflet marker
+	const greenIcon = new L.Icon({
+		iconUrl:
+			"https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+		shadowUrl:
+			"https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	})
+
+	const redIcon = new L.Icon({
+		iconUrl:
+			"https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+		shadowUrl:
+			"https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	})
+
 	// State to hold the roads
 	const [mapState, setMapState] = useState(null)
 	const [shortestRoute, setShortestRoute] = useState(null)
@@ -18,6 +42,9 @@ const App = () => {
 	const [endMarkerCoords, setEndMarkerCoords] = useState({
 		lat: -25.7521842,
 		lng: 28.2328684
+	})
+	const [showErrorMessage, setShowErrorMessage] = useState({
+		errorMessage: false
 	})
 
 	/*
@@ -139,7 +166,7 @@ const App = () => {
 	*/
 	const handleSearchLocation = (formData) => {
 		fetch(
-			`https://nominatim.openstreetmap.org/search?country=&q=${formData.startLocation}, South Africa&format=geojson`
+			`https://nominatim.openstreetmap.org/search?country=&q=${formData.startLocation}, Pretoria, South Africa&format=geojson`
 		)
 			.then((response) => response.json())
 			.then((data) => {
@@ -152,7 +179,7 @@ const App = () => {
 			})
 
 		fetch(
-			`https://nominatim.openstreetmap.org/search?country=&q=${formData.endLocation}, South Africa&format=geojson`
+			`https://nominatim.openstreetmap.org/search?country=&q=${formData.endLocation}, Pretoria, South Africa&format=geojson`
 		)
 			.then((response) => response.json())
 			.then((data) => {
@@ -167,7 +194,22 @@ const App = () => {
 
 	return (
 		<div>
-			<SearchLocation handleSearchLocation={handleSearchLocation} />
+			<div id="top-bar">
+				<div className="side-text">
+					<h1>Safe Routes</h1>
+				</div>
+				<SearchLocation
+					handleSearchLocation={handleSearchLocation}
+					setShowErrorMessage={setShowErrorMessage}
+				/>
+				<div className="side-text">
+					{showErrorMessage.errorMessage ? (
+						<h2>Please enter a Start & End Location</h2>
+					) : (
+						<h2>Navigate a route in Hatfield</h2>
+					)}
+				</div>
+			</div>
 			<MapContainer ref={setMapState} center={[-25.7487, 28.238]} zoom={15}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -176,12 +218,14 @@ const App = () => {
 				<ShortestRoute shortestRoute={shortestRoute} />
 				<SafestRoute safestRoute={safestRoute} />
 				<Marker
+					icon={greenIcon}
 					draggable={true}
 					eventHandlers={startMarkerEventHandlers}
 					position={startMarkerCoords}
 					ref={startMarkerRef}
 				></Marker>
 				<Marker
+					icon={redIcon}
 					draggable={true}
 					eventHandlers={endMarkerEventHandlers}
 					position={endMarkerCoords}
--- src/components/SearchLocation.jsx
+++ src/components/SearchLocation.jsx
@@ -1,8 +1,10 @@
+import { Button, TextField } from "@mui/material"
 import React, { useState } from "react"
 
 /*
     Props:
     handleSearchLocation
+	setShowErrorMessage
 */
 
 const SearchLocation = (props) => {
@@ -27,28 +29,59 @@ const SearchLocation = (props) => {
 		Send the searchValue data to the App.jsx file
 	*/
 	const handleSubmit = (event) => {
-		event.preventDefault()
-		// Rest form to be empty
-		props.handleSearchLocation(searchValue)
+		if (searchValue.startLocation === "" || searchValue.endLocation === "") {
+			event.preventDefault()
+			props.setShowErrorMessage({
+				errorMessage: true
+			})
+		} else {
+			event.preventDefault()
+
+			// Reset form to be empty
+			setSearchValue({
+				startLocation: "",
+				endLocation: ""
+			})
+
+			// Set the error message to false
+			props.setShowErrorMessage({
+				errorMessage: false
+			})
+
+			// Send the search values to the App
+			props.handleSearchLocation(searchValue)
+		}
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				name="startLocation"
-				placeholder="Start Location"
-				type="text"
-				value={searchValue.startLocation}
-				onChange={handleChange}
-			></input>
-			<input
-				name="endLocation"
-				placeholder="End Location"
-				type="text"
-				value={searchValue.endLocation}
-				onChange={handleChange}
-			></input>
-			<input type="submit" value="Find Route"></input>
+		<form id="start-end-form" onSubmit={handleSubmit}>
+			<div>
+				<TextField
+					size="small"
+					id="start-location"
+					variant="outlined"
+					margin="dense"
+					name="startLocation"
+					label="Start Location"
+					type="text"
+					value={searchValue.startLocation}
+					onChange={handleChange}
+				></TextField>
+				<TextField
+					size="small"
+					id="end-location"
+					variant="outlined"
+					margin="dense"
+					name="endLocation"
+					label="End Location"
+					type="text"
+					value={searchValue.endLocation}
+					onChange={handleChange}
+				></TextField>
+			</div>
+			<Button variant="outlined" size="small" type="submit">
+				Find Route
+			</Button>
 		</form>
 	)
 }
